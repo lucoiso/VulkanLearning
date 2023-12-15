1a44a1e465dbba00b4e065ed7ae7b238eb637c38
lucoiso <lucas.oliveiravb@gmail.com>
2023-12-15 19:28:46 +0000

    independent lib

--- Source/CMake/Utils.cmake
+++
@@ -1,8 +0,0 @@
-# Author: Lucas Vilas-Boas
-# Year: 2023
-# Repo: https://github.com/lucoiso/VulkanRenderer
-
-# Replace '\' with '\\'
-FUNCTION(REPLACE_BACKSLASHES_IN_VARIABLE INPUT_VARIABLE)
-	STRING(REPLACE "\\" "\\\\" ${INPUT_VARIABLE} "${${INPUT_VARIABLE}}")
-ENDFUNCTION()
